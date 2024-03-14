38a3bbe259ba334d03eeab83853f25917f5f48d4
Ilya M. <gvardeec-13@bk.ru>
2024-03-14 12:14:42 +0300

    Homework is done

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿string str_ori = "Я люблю учиться";
+
+string str_res = "";
+
+string[] words = str_ori.ToLower().Split(' ');
+
+for (int i = words.Length - 1; i >= 0; i--)
+{
+    str_res += words[i];
+    if (i != 0)
+    {
+        str_res += " ";
+    }
+}
+
+Console.WriteLine(str_res);
